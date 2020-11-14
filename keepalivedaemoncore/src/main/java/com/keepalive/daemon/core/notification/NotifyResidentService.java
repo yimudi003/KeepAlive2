86ecfcb4f984b3ea57b5dbfa787cf3ce9a76831c
zhaozhangyun <zhaozhangyun@gmail.com>
2020-11-14 11:31:38 +0800

    .

--- keepalivedaemoncore/src/main/java/com/keepalive/daemon/core/notification/NotifyResidentService.java
+++ keepalivedaemoncore/src/main/java/com/keepalive/daemon/core/notification/NotifyResidentService.java
@@ -17,7 +17,6 @@ public class NotifyResidentService extends KeepAliveService {
 
     @Override
     public final void onCreate() {
-        ServiceHolder.fireService(this, DaemonService.class, false);
         doStart();
         super.onCreate();
     }
